52bafa4e5229c4e5f5939907b2625df6a4230180
w-hosinski <woj.hosinski@gmail.com>
2022-01-25 12:27:40 +0100

    CSS+cleaner code
    CSS fertiggestellt
    Code formatierung leserlicher gemacht
    Unique page feature

--- src/Wallpaper.js
+++ src/Wallpaper.js
@@ -3,43 +3,50 @@ import './wallpaper.scss'
 
 const Wallpaper = () => {
     const [picDataArr, setPicDataArr] = useState([])
+    const [pageNum, setPageNum] = useState(1)
 
     const fetchWallpaper = async() => {
-        const response = await fetch("https://api.unsplash.com/photos/?client_id=MCWF9paqDNakL9OxfbhiK64aCpfjRwz86ETp6NRtY-A")
+        const response = await fetch(`https://api.unsplash.com/photos/?page=${pageNum}&client_id=MCWF9paqDNakL9OxfbhiK64aCpfjRwz86ETp6NRtY-A`)
         const images = await response.json()
         return images   
     }
 
     const displayWallpaper = async () => {
+        setPageNum(pageNum+1)
         let data = await fetchWallpaper()
         console.log(data)
         let tempPicDataArr = []
         for(let i=0;i<9;i++) {
-            tempPicDataArr.push({url: data[i].urls.regular, alt: data[i].alt_description, name: data[i].user.name, desc: data[i].description, download: data[i].links.download})
+            tempPicDataArr.push({
+                url: data[i].urls.regular, 
+                alt: data[i].alt_description, 
+                name: data[i].user.name, 
+                desc: data[i].description, 
+                download: data[i].links.download
+            })
         }
         setPicDataArr([...picDataArr,...tempPicDataArr])
       }
+    
     useEffect(() => {
         displayWallpaper()  
     },[]) 
 
-return(<div>
-    <h1>Unsplash Wallpaper Downloader</h1>
-    <div className="container">
-        {picDataArr.map(item => {
-            return (
-            <li key={item.url}><img src={item.url} width="3200" height="1800" alt={item.alt}></img>
-            <p className="toolTip" id="name">{item.name}</p>
-            <p className="toolTip" id="desc">{item.desc}</p>
-            <a className="toolTip" id="download" href={item.download} download={item.desc+".jpg"}>Download</a>
-            </li>
-            )
-            
-        })}
-    
-    </div>
-    <button className="loadMore" onClick={displayWallpaper}>Load More</button>
-    </div>
-)
-}
+    return(<div>
+        <h1>Unsplash Wallpaper Downloader</h1>
+        <div className="container">
+            {picDataArr.map(item => {
+                return (
+                <li key={item.url}><img src={item.url} width="3200" height="1800" alt={item.alt}></img>
+                <p className="toolTip" id="name">{item.name}</p>
+                <p className="toolTip" id="desc">{item.desc}</p>
+                <a className="toolTip" id="download" href={item.download} download={item.desc+".jpg"}>Download</a>
+                </li>
+                )  
+            })}
+        </div>
+        <button className="loadMore" onClick={displayWallpaper}>Load More</button>
+    </div>)
+    }
+
 export default Wallpaper
